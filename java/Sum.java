0d0e10d2832dc12cef37f49bc9c6076c9d8c3064
chickenplace <jakeMsaperfad@gmail.com>
2026-01-15 08:12:52 -0600

    Update Sum.java

--- java/Sum.java
+++ java/Sum.java
@@ -12,6 +12,7 @@ public class Sum {
     }
 
     public static void main(String args[]) {
+        System.out.println("What would you like to sum up to? The program will sum all integers from 1 to your number.");
         Scanner in = new Scanner(System.in);
         int n = Integer.parseInt(in.nextLine());
         System.out.printf("%d\n", sum(n));    
