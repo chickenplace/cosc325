3691116f36bce76c364b1bb3d0adaa54ef4ab580
chickenplace <jakeMsaperfad@gmail.com>
2026-02-03 09:53:30 -0600

    Update parser.c

--- c/lab3/parser.c
+++ c/lab3/parser.c
@@ -1,5 +1,8 @@
 #include "lexer.c" //def not the right way to do this but ok for now
 
+void line(); //forward declaration
+void statement(); //forward declaration
+
 int main() {
    /* Open the input data file and process its contents */
    if  ((in_fp = fopen("front.in", "r")) == NULL)
@@ -8,6 +11,18 @@ int main() {
      getChar();
      do   {
        lex(); //lex calls getnonblank which calls getchar
+       line();
     }  while (nextToken != EOF);
   }
+}
+
+void line() {
+    if (nextToken == NUMBER) { //If line number then handle
+        //consume token by looking at line numver and storing the statement that follows in the right place in our new program
+        //but not for this assignment
+        //Call lex() to get next token;
+        lex();
+    } else { //Else its just a statement
+
+    }
 }
